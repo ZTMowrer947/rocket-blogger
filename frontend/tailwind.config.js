70999dc16b83c9a4c691f817645767cc0ab4773a
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-19 14:45:26 -0500

    Add and configure Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    '../templates/**/*.tera',
+    'lib/**/*.{css,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
