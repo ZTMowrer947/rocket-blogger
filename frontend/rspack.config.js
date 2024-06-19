d40d5df669f472b1fe52cb5dda14027bb4db0d76
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-19 15:03:48 -0500

    Update rspack output

--- frontend/rspack.config.js
+++ frontend/rspack.config.js
@@ -1,7 +1,12 @@
+import { resolve } from 'node:path';
+
 /** @type {import('@rspack/cli').Configuration} */
 export default {
   entry: {
     main: './lib/index.css'
+  },
+  output: {
+    path: resolve(__dirname, '..', 'public', 'assets')
   },
   module: {
     rules: [
