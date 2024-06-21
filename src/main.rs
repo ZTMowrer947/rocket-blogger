b7acb745f55b3c9417f2603faedecc9ec69e7d4c
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-20 19:56:36 -0500

    Add dotenv init and db fairing
    Completes process of wiring Diesel to Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use] extern crate rocket;
 use rocket::fs::{FileServer, relative};
+use rocket_db_pools::Database;
 use rocket_dyn_templates::Template;
 
 pub(crate) mod schema;
@@ -9,7 +10,12 @@ pub(crate) mod db;
 
 #[launch]
 fn rocket() -> _ {
+    // Load env vars from .env
+    dotenvy::dotenv().ok();
+
+    // Setup database, templates, static files, and routes
     rocket::build()
+        .attach(db::Blogger::init())
         .attach(Template::fairing())
         .mount("/public", FileServer::from(relative!("public")))
         .mount("/", routes::prelude::all_routes())
