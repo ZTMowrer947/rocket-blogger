2c9a88589cc41d62f657a4ba29b26a76b2ed7665
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-21 08:24:43 -0500

    Correct tailwind content path

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    '../templates/**/*.{html.tera}'
+    '../templates/**/*.html.tera'
   ],
   theme: {
     extend: {},
