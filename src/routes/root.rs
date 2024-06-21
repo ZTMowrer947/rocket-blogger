9e59288a03d28b594b1bd230b9b4cd640b1be91a
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-21 10:50:01 -0500

    Add placeholder nonces for assets

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -2,5 +2,8 @@ use rocket_dyn_templates::{context, Template};
 
 #[get("/")]
 pub fn index() -> Template {
-    Template::render("index", context! {})
+    Template::render("index", context! {
+        style_nonces: vec!["style_placeholder"],
+        script_nonces: vec!["script_placeholder"]
+    })
 }
