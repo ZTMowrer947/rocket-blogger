b88d8f124f1c7bbf5e88b377779ab23997e837e8
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-24 08:05:50 -0500

    Add PostInput struct

--- src/routes/create.rs
+++ src/routes/create.rs
@@ -1,7 +1,18 @@
+use diesel::Insertable;
 use rocket::http::Status;
 use rocket_dyn_templates::Template;
 
-use crate::csp::NonceContext;
+use crate::{csp::NonceContext, schema::posts};
+
+// Form data for creating a new post
+#[derive(FromForm, Insertable)]
+#[diesel(table_name = posts)]
+pub struct PostInput<'r> {
+    #[field(validate = neq("").or_else(msg!("Title must not be empty")))]
+    title: &'r str,
+    #[field(validate = neq("").or_else(msg!("Body must not be empty")))]
+    body: &'r str,
+}
 
 #[get("/new-post")]
 pub fn new_post_form(nonces: &NonceContext) -> Template {
