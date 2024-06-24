3b7f089f8d361d869d8e20670cecfa2fff4dfc55
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-24 10:38:47 -0500

    Partially implement post creation route

--- src/routes/create.rs
+++ src/routes/create.rs
@@ -1,8 +1,12 @@
 use diesel::Insertable;
-use rocket::http::Status;
+use rocket::form::{Contextual, Form, FromForm};
+use rocket::response::Redirect;
+use rocket_db_pools::{Connection, diesel::insert_into};
 use rocket_dyn_templates::Template;
 
+use crate::db::Blogger;
 use crate::{csp::NonceContext, schema::posts};
+use crate::models::post::Post;
 
 // Form data for creating a new post
 #[derive(FromForm, Insertable)]
@@ -19,7 +23,28 @@ pub fn new_post_form(nonces: &NonceContext) -> Template {
     Template::render("new", nonces)
 }
 
-#[post("/new-post")]
-pub fn new_post() -> Status {
-    Status::NotImplemented
+#[post("/new-post", data = "<input>")]
+pub async fn new_post(
+    mut db: Connection<Blogger>,
+    nonces: &NonceContext,
+    input: Form<Contextual<PostInput<'_>, '_>>) -> Result<Redirect, Template> {
+    // Parse form
+    if let Some(ref post_data) = input.value {
+        use rocket_db_pools::diesel::prelude::*;
+
+        // If successful, create post from input data
+        insert_into(posts::table)
+            .values(post_data)
+            .returning(Post::as_returning())
+            .get_result(&mut db)
+            .await
+            .expect("Unexpected error during post creation");
+
+        Ok(Redirect::to(uri!(super::root::index)))
+    } else {
+        println!("Validation context: {:?}", input.context);
+
+        // TODO: Pass validation errors back to form
+        Err(Template::render("new", nonces))
+    }
 }
